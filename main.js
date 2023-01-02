5c3fd2d0b02e79ab376639dadbb611091dfd19a3
Verena Sousa <verena.ads19@hotmail.com>
2023-01-02 13:17:38 -0300

    Parte do código javascript conseguido

--- main.js
+++ main.js
@@ -3,45 +3,35 @@
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
 const btnOpen = document.querySelector('#btnOpen')
-const btnOpenAgain = document.querySelector('#btnOpenAgain')
-// let message = ["Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.", "Não é o que você fala que te define, mas sim o que você faz", "Sua força estará em permanecerem calmos e terem confiança"]
-// let randonPhrases = message[Math.floor((Math.random() * message.length))]
+// const btnOpenAgain = document.querySelector('#btnOpenAgain')
+let message = ["Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.", "Não é o que você fala que te define, mas sim o que você faz", "Sua força estará em permanecerem calmos e terem confiança"]
+let randonPhrases = message[Math.floor((Math.random() * message.length))]
 
-// console.log(randonPhrases.message)
+console.log(randonPhrases)
 
 //Eventos
 
-btnOpen.addEventListener('click', handleOpenClick)
-btnOpenAgain.addEventListener('click', handleOpenAgainClick)
+btnOpen.addEventListener('click', handleClick)
+// btnOpenAgain.addEventListener('click', handleOpenAgainClick)
 
-
-
-//Funções
-
-function handleOpenClick(event) {
+function handleClick(event) {
     event.preventDefault()
 
+    toggleScreen()
+
+    screen2.querySelector(".message").innerText = randonPhrases
+
     
-
-    let btnOpen = document.querySelector('#btnOpen')
-
-    if(btnOpen == onclick){
-        toggleScreen()
-    }
-
     console.log(btnOpen)
-
-
 }
+
 
 
 
 
 
 function toggleScreen(){
-
     screen1.classList.toggle("hide")
     screen2.classList.toggle("hide")
-}
 
-// toggleScreen();
+};
